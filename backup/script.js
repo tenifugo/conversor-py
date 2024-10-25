fcd59dc240147891fa69406452a45bd4ce444623
tenifugo <rafadeaquino@hotmail.com>
2024-10-24 22:39:38 -0300

    formulario funcionando

--- backup/script.js
+++ backup/script.js
@@ -1,80 +1,94 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document
-    .querySelector('button[type="submit"]')
-    .addEventListener('click', function (event) {
+  const submitButton = document.querySelector('button[type="submit"]');
+
+  if (submitButton) {
+    submitButton.addEventListener('click', function (event) {
       event.preventDefault(); // Impede o envio padrão do formulário
 
-      const rows = document.querySelectorAll('#socioContainer tbody tr');
       const data = {
-        empresa: {
-          razaoSocial: document.querySelector('input[name="razaoSocial"]')
-            .value,
-          cnpj: document.querySelector('input[name="cnpj"]').value,
-          nomeFantasia: document.querySelector('input[name="nomeFantasia"]')
-            .value,
-          dataAbertura: document.querySelector('input[name="dataAbertura"]')
-            .value,
-          atividadePrincipal: document.querySelector(
-            'input[name="atividadePrincipal"]',
-          ).value,
-          endereco: document.querySelector('input[name="endereco"]').value,
-          site: document.querySelector('input[name="site"]').value,
-          arranjoSocietario:
-            document.querySelector('input[name="arranjo"]:checked')?.value ||
-            null,
-          compliance:
-            document.querySelector('input[name="compliance"]:checked')?.value ||
-            null,
-          complianceOfficer: document.querySelector(
-            'input[name="complianceOfficer"]',
-          ).value,
-          email: document.querySelector('input[name="email"]').value,
-          codigoConduta:
-            document.querySelector('input[name="codigoConduta"]:checked')
-              ?.value || null,
-          pldft:
-            document.querySelector('input[name="pldft"]:checked')?.value ||
-            null,
-          socios: [],
-        },
+        accessKey: 'dfb70fb0-b31c-4e05-b6aa-e2a42389a4cc', // Adicionando a accessKey
+        $razaoSocial: document.querySelector('input[name="$razaoSocial"]')
+          .value,
+        $cnpj: document.querySelector('input[name="$cnpj"]').value,
+        $nomeFantasia: document.querySelector('input[name="$nomeFantasia"]')
+          .value,
+        $dataAbertura: document.querySelector('input[name="$dataAbertura"]')
+          .value,
+        $atividadePrincipal: document.querySelector(
+          'input[name="$atividadePrincipal"]',
+        ).value,
+        $endereco: document.querySelector('input[name="$endereco"]').value,
+        $site: document.querySelector('input[name="$site"]').value,
+        $arranjo:
+          document.querySelector('input[name="$arranjo"]:checked')?.value ||
+          null,
+        $compliance:
+          document.querySelector('input[name="$compliance"]:checked')?.value ||
+          null,
+        $complianceOfficer: document.querySelector(
+          'input[name="$complianceOfficer"]',
+        ).value,
+        $email: document.querySelector('input[name="$email"]').value,
+        $codigoConduta:
+          document.querySelector('input[name="$codigoConduta"]:checked')
+            ?.value || null,
+        $pldft:
+          document.querySelector('input[name="$pldft"]:checked')?.value || null,
+        socios: [],
       };
 
-      let valid = true; // Flag para verificar se todos os campos obrigatórios estão preenchidos
-
+      // Adiciona sócios ao array se os campos estiverem preenchidos
+      const rows = document.querySelectorAll('#socioContainer tbody tr');
       rows.forEach((row) => {
-        const nome = row.querySelector('.nomeSocio').value;
-        const cpf = row.querySelector('.cpfSocio').value;
-        const dataNascimento = row.querySelector('.dataNascimento').value;
-        const percentual = row.querySelector('.percentualSocietario').value;
+        const nome = row.querySelector('.nomeSocio')?.value || '';
+        const cpf = row.querySelector('.cpfSocio')?.value || '';
+        const dataNascimento =
+          row.querySelector('.dataNascimento')?.value || '';
+        const percentual =
+          row.querySelector('.percentualSocietario')?.value || '';
 
-        // Adiciona sócios apenas se algum campo estiver preenchido
         if (nome || cpf || dataNascimento || percentual) {
-          data.empresa.socios.push({ nome, cpf, dataNascimento, percentual });
+          data.socios.push({ nome, cpf, dataNascimento, percentual });
         }
       });
 
-      // Verifique se a empresa tem sócios
-      if (data.empresa.socios.length === 0) {
-        alert('Nenhum sócio foi adicionado. Isso está correto?');
+      // Validação simples
+      if (
+        !data['$razaoSocial'] ||
+        !data['$cnpj'] ||
+        !data['$nomeFantasia'] ||
+        !data['$email']
+      ) {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+        return;
       }
 
-      if (!valid) {
-        alert('Por favor, preencha todos os campos obrigatórios dos sócios.');
-        return; // Evita o download se algum campo estiver vazio
-      }
-
-      // Debug: Verifique os dados coletados
-      console.log(data);
-
-      const jsonData = JSON.stringify(data, null, 2);
-      const blob = new Blob([jsonData], { type: 'application/json' });
-      const link = document.createElement('a');
-      const url = URL.createObjectURL(blob);
-
-      link.setAttribute('href', url);
-      link.setAttribute('download', 'dados_socios.json');
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+      // Envio dos dados para o Static Forms
+      console.log('Dados enviados:', JSON.stringify(data));
+      fetch('https://api.staticforms.xyz/submit', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert('Formulário enviado com sucesso!');
+            document.getElementById('kycForm').reset(); // Limpa o formulário
+          } else {
+            return response.text().then((text) => {
+              console.error('Erro do servidor:', text);
+              alert('Erro ao enviar o formulário: ' + text);
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Erro:', error);
+          alert('Ocorreu um erro ao enviar o formulário.');
+        });
     });
+  } else {
+    console.error('Botão de envio não encontrado.');
+  }
 });
--- script.js
+++ script.js
@@ -1,77 +1,75 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document
-    .querySelector('button[type="submit"]')
-    .addEventListener('click', function (event) {
+  const submitButton = document.querySelector('button[type="submit"]');
+
+  if (submitButton) {
+    submitButton.addEventListener('click', function (event) {
       event.preventDefault(); // Impede o envio padrão do formulário
 
-      const rows = document.querySelectorAll('#socioContainer tbody tr');
       const data = {
-        razaoSocial: document.querySelector('input[name="razaoSocial"]').value,
-        cnpj: document.querySelector('input[name="cnpj"]').value,
-        nomeFantasia: document.querySelector('input[name="nomeFantasia"]')
+        accessKey: 'dfb70fb0-b31c-4e05-b6aa-e2a42389a4cc', // Adicionando a accessKey
+        $razaoSocial: document.querySelector('input[name="$razaoSocial"]')
           .value,
-        dataAbertura: document.querySelector('input[name="dataAbertura"]')
+        $cnpj: document.querySelector('input[name="$cnpj"]').value,
+        $nomeFantasia: document.querySelector('input[name="$nomeFantasia"]')
           .value,
-        atividadePrincipal: document.querySelector(
-          'input[name="atividadePrincipal"]',
+        $dataAbertura: document.querySelector('input[name="$dataAbertura"]')
+          .value,
+        $atividadePrincipal: document.querySelector(
+          'input[name="$atividadePrincipal"]',
         ).value,
-        endereco: document.querySelector('input[name="endereco"]').value,
-        site: document.querySelector('input[name="site"]').value,
-        arranjo:
-          document.querySelector('input[name="arranjo"]:checked')?.value ||
+        $endereco: document.querySelector('input[name="$endereco"]').value,
+        $site: document.querySelector('input[name="$site"]').value,
+        $arranjo:
+          document.querySelector('input[name="$arranjo"]:checked')?.value ||
           null,
-        compliance:
-          document.querySelector('input[name="compliance"]:checked')?.value ||
+        $compliance:
+          document.querySelector('input[name="$compliance"]:checked')?.value ||
           null,
-        complianceOfficer: document.querySelector(
-          'input[name="complianceOfficer"]',
+        $complianceOfficer: document.querySelector(
+          'input[name="$complianceOfficer"]',
         ).value,
-        email: document.querySelector('input[name="email"]').value,
-        codigoConduta:
-          document.querySelector('input[name="codigoConduta"]:checked')
+        $email: document.querySelector('input[name="$email"]').value,
+        $codigoConduta:
+          document.querySelector('input[name="$codigoConduta"]:checked')
             ?.value || null,
-        pldft:
-          document.querySelector('input[name="pldft"]:checked')?.value || null,
+        $pldft:
+          document.querySelector('input[name="$pldft"]:checked')?.value || null,
         socios: [],
       };
 
-      let valid = true; // Flag para verificar se todos os campos obrigatórios estão preenchidos
-
+      // Adiciona sócios ao array se os campos estiverem preenchidos
+      const rows = document.querySelectorAll('#socioContainer tbody tr');
       rows.forEach((row) => {
-        const nome = row.querySelector('.nomeSocio').value;
-        const cpf = row.querySelector('.cpfSocio').value;
-        const dataNascimento = row.querySelector('.dataNascimento').value;
-        const percentual = row.querySelector('.percentualSocietario').value;
+        const nome = row.querySelector('.nomeSocio')?.value || '';
+        const cpf = row.querySelector('.cpfSocio')?.value || '';
+        const dataNascimento =
+          row.querySelector('.dataNascimento')?.value || '';
+        const percentual =
+          row.querySelector('.percentualSocietario')?.value || '';
 
-        // Adiciona sócios apenas se algum campo estiver preenchido
         if (nome || cpf || dataNascimento || percentual) {
           data.socios.push({ nome, cpf, dataNascimento, percentual });
         }
       });
 
-      // Verifique se a empresa tem sócios
-      if (data.socios.length === 0) {
-        alert('Nenhum sócio foi adicionado. Isso está correto?');
-      }
-
       // Validação simples
       if (
-        !data.razaoSocial ||
-        !data.cnpj ||
-        !data.nomeFantasia ||
-        !data.email
+        !data['$razaoSocial'] ||
+        !data['$cnpj'] ||
+        !data['$nomeFantasia'] ||
+        !data['$email']
       ) {
         alert('Por favor, preencha todos os campos obrigatórios.');
         return;
       }
 
       // Envio dos dados para o Static Forms
-      fetch('https://staticforms.xyz/form', {
+      console.log('Dados enviados:', JSON.stringify(data));
+      fetch('https://api.staticforms.xyz/submit', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
           'Content-Type': 'application/json',
-          Authorization: 'Bearer dfb70fb0-b31c-4e05-b6aa-e2a42389a4cc', // Sua chave
         },
       })
         .then((response) => {
@@ -79,14 +77,18 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Formulário enviado com sucesso!');
             document.getElementById('kycForm').reset(); // Limpa o formulário
           } else {
-            return response.json().then((err) => {
-              throw new Error(err.message || 'Erro ao enviar o formulário.');
+            return response.text().then((text) => {
+              console.error('Erro do servidor:', text);
+              alert('Erro ao enviar o formulário: ' + text);
             });
           }
         })
         .catch((error) => {
           console.error('Erro:', error);
-          alert('Ocorreu um erro ao enviar o formulário: ' + error.message);
+          alert('Ocorreu um erro ao enviar o formulário.');
         });
     });
+  } else {
+    console.error('Botão de envio não encontrado.');
+  }
 });
