d85a5645db1cef655b7b95273ccb475393fba9f4
tenifugo <rafadeaquino@hotmail.com>
2024-10-27 22:50:54 -0300

    conversor atualizado, com os campos novos adicionados

--- conversor py/conversor.py
+++ conversor py/conversor.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 from docx import Document
 from docx.shared import Pt
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT  # Para centralização
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 import re
 
 def process_text():
@@ -31,19 +31,30 @@ def format_key(key):
         'cidade': 'Cidade:',
         'estado': 'Estado:',
         'site': 'Site da Empresa:',
-        'arranjo': 'O Cliente é membro de um ou mais Arranjo(s) Societário(s)?',
-        'compliance': 'A empresa possui departamento de Compliance? Se sim, informar nome do(a) Compliance Officer e e-mail:',
+        'arranjo_selecionado': 'O Cliente é membro de um ou mais Arranjo(s) Societário(s)?',
+        'descricao_arranjo': 'Descrição do Arranjo:',
+        'nome_socio1': 'Nome Completo dos Sócios/Diretores/Administradores/Representantes Legais 1:',
+        'cpf_socio1': 'CPF/CNPJ 1:',
+        'data_nascimento1': 'Data de Nascimento 1:',
+        'percentual_societario1': 'Percentual Societário 1:',
+        'us_person1': 'É US Person 1?',
+        'compliance_selecionado': 'A empresa possui departamento de Compliance?',
         'compliance_officer': 'Nome do(a) Compliance Officer e e-mail:',
-        'codigo_conduta': 'A empresa possui Código de Conduta/Ética?',
-        'pldft': 'A Empresa possui políticas e procedimentos de prevenção a fraudes?',
-        'organograma': 'Existem outras empresas que constam no organograma da organização?',
+        'email_compliance': 'E-mail do Compliance Officer:',
+        'codigo_conduta_selecionado': 'A empresa possui Código de Conduta/Ética?',
+        'pldft_selecionado': 'A Empresa possui políticas e procedimentos de prevenção a fraudes?',
+        'organograma_selecionado': 'Existem outras empresas que constam no organograma da organização?',
         'detalhes_organograma': 'Por favor, descreva os detalhes do organograma:',
-        'autuacao': 'A empresa ou qualquer outra pertencente ao mesmo grupo econômico já foi autuada?',
-        'outra_sociedade': 'Os sócios fazem parte de outras sociedades/empresas?',
-        'processos': 'A empresa ou seus sócios já foram objeto de processos administrativos ou criminais?',
-        'pep': 'A empresa ou seus sócios são considerados PEP (Pessoas Expostas Politicamente)?',
-        'citado_midia': 'A empresa e/ou seus sócios, é (são) citada(os) em mídia por suspeitas de atividades criminais?',
-        'percentual_dinheiro': 'Qual o percentual que representa dinheiro em espécie?',
+        'autuacao_selecionado': 'A empresa ou qualquer outra pertencente ao mesmo grupo econômico já foi autuada?',
+        'detalhes_autuacao': 'Detalhes da Autuação:',
+        'outra_sociedade_selecionado': 'Os sócios fazem parte de outras sociedades/empresas?',
+        'detalhes_outra_sociedade': 'Detalhes da Outra Sociedade:',
+        'processos_selecionado': 'A empresa ou seus sócios já foram objeto de processos administrativos ou criminais?',
+        'detalhes_processos': 'Detalhes dos Processos:',
+        'pep_selecionado': 'A empresa ou seus sócios são considerados PEP (Pessoas Expostas Politicamente)?',
+        'detalhes_pep': 'Detalhes PEP:',
+        'citado_midia_selecionado': 'A empresa e/ou seus sócios, é (são) citada(os) em mídia por suspeitas de atividades criminais?',
+        'percentual_dinheiro_selecionado': 'Qual o percentual que representa dinheiro em espécie?',
         'gestao_empresa': 'Por quem é feita a gestão da empresa?',
         'atividades_empresa': 'Qual(is) a(s) atividade(s) da empresa?',
         'origem_recursos': 'Se a empresa foi constituída há menos de 2 anos, informar a origem dos recursos:',
@@ -60,7 +71,6 @@ def format_key(key):
         'servicos': 'Objeto da Parceria: Quais serviços do 3X Pay serão utilizados? (Transações via Pix, Transações Cartão de Crédito, APIs, Emissão de Boletos)'
     }
 
-    # Adicionando mapeamento para múltiplos sócios
     for i in range(2, 6):
         question_mapping[f'nome_socio{i}'] = f'Nome Completo dos Sócios/Diretores/Administradores/Representantes Legais {i}:'
         question_mapping[f'cpf_socio{i}'] = f'CPF/CNPJ {i}:'
@@ -80,12 +90,11 @@ def format_value(value, key):
 def save_to_word(data):
     document = Document()
     
-    # Título
     title = document.add_heading('Relatório de Dados do Formulário', level=1)
-    title.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # Centralizar o título
+    title.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     run = title.runs[0]
-    run.font.size = Pt(14)  # Aumentar o tamanho da fonte
-    run.font.name = 'Arial Narrow'  # Definir a fonte como Arial Narrow
+    run.font.size = Pt(14)
+    run.font.name = 'Arial Narrow'
 
     document.add_paragraph()  # Espaçamento
 
@@ -95,26 +104,36 @@ def save_to_word(data):
         formatted_key = format_key(key)
         formatted_value = format_value(value, key)
 
-        # Adicionar pergunta em negrito
         paragraph = document.add_paragraph()
         run = paragraph.add_run(f"{formatted_key}")
         run.bold = True
-        run.font.name = 'Arial Narrow'  # Definir a fonte como Arial Narrow
+        run.font.name = 'Arial Narrow'
         
-        # Adicionar a resposta em um novo parágrafo
         response_paragraph = document.add_paragraph(formatted_value)
-        response_paragraph.style.font.name = 'Arial Narrow'  # Definir a fonte como Arial Narrow
+        response_paragraph.style.font.name = 'Arial Narrow'
 
     document.save(nome_arquivo)
 
 # Configuração da interface gráfica
 root = tk.Tk()
+root.geometry("600x500")  # Tamanho da janela
 root.title("Formulário para Word")
+root.configure(bg="#f7f7f7")  # Cor de fundo da janela
 
-text_area = tk.Text(root, height=20, width=50)
-text_area.pack(pady=20)
+# Frame principal
+frame = tk.Frame(root, bg="#f7f7f7")
+frame.pack(pady=20)
 
-process_button = tk.Button(root, text="Exportar para Word", command=process_text)
+# Texto de instrução
+instruction_label = tk.Label(frame, text="Insira os dados abaixo:", bg="#f7f7f7", font=("Arial", 12, "bold"))
+instruction_label.pack()
+
+# Área de texto
+text_area = tk.Text(frame, height=20, width=70, bg="#ffffff", fg="#333333", font=("Arial", 12), borderwidth=2, relief="solid")
+text_area.pack(pady=10)
+
+# Botão
+process_button = tk.Button(frame, text="Exportar para Word", command=process_text, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"), relief="raised")
 process_button.pack(pady=10)
 
 # Executar a aplicação
